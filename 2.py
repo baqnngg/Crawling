0b3d4c162fa282c262bd75ea9959e031500ebd5d
baqnngg <il8696546@gmail.com>
2025-05-19 16:28:09 +0900

    5_19

--- 2.py
+++ 2.py
@@ -12,10 +12,12 @@ soup = BeautifulSoup(html, "html.parser")
 
 # 맛집 이름 수집
 restaurants = []
+link2 = []
 for link in soup.find_all("a", href=True):
     if "/Restaurant_Review-" in link["href"]:
         name = link.get_text(strip=True)
         if name and name not in restaurants:
+            link2.append(link.get_text)
             restaurants.append(name)
 
 # 출력
@@ -26,3 +28,4 @@ for i, name in enumerate(restaurants, 1):
 with open("restaurant_names.txt", "w", encoding="utf-8") as f:
         for name in restaurants:
             f.write(name + "\n")
+print(link2)
